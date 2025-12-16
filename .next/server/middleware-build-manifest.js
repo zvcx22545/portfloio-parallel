dd883ac54d5a85c00cd8e1eedd763d89eb6e2c60
@chisanupong <chisanupong.limsakul@gmail.com>
2025-12-16 10:52:12 +0700

    build: Generate Next.js output and update package dependencies.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,4 +1,4 @@
-self.__BUILD_MANIFEST = {
+globalThis.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
@@ -9,12 +9,13 @@ self.__BUILD_MANIFEST = {
     "static/chunks/webpack.js",
     "static/chunks/main-app.js"
   ],
+  "rootMainFilesTree": {},
   "pages": {
     "/_app": []
   },
   "ampFirstPages": []
 };
-self.__BUILD_MANIFEST.lowPriorityFiles = [
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
 ,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
 
